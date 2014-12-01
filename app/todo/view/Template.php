e8f5fecd5fbe491c452d347c3ac724e4ccfe8a20
jpsi5 <jsimon@clemson.edu>
2014-12-01 14:53:50 -0500

    Updated helper functions to set the crruent module | Preparing for handling mutlitple templates usage

--- app/todo/view/Template.php
+++ app/todo/view/Template.php
@@ -5,14 +5,17 @@ class Todo_View_Template extends Core_View_Template{
     function render() {
         extract($this->variables);
 
+        # Header
         if (file_exists(ROOT  . 'app' . DS . 'todo' . DS . 'view' . DS . $this->controller . DS . 'header.php')) {
             include (ROOT  . 'app' . DS . 'todo' . DS . 'view' . DS . $this->controller . DS . 'header.php');
         } else {
-            //include (ROOT . DS . 'application' . DS . 'views' . DS . 'header.php');
+            //include (ROOT . 'app' . DS . 'views' . DS . 'header.php');
         }
 
+        # Content
         include (ROOT . 'app' . DS . 'todo' . DS . 'view' . DS . $this->controller . DS . $this->action . '.php');
 
+        # Footer
         if (file_exists(ROOT  . 'app' . DS . 'todo' . DS . 'view' . DS . $this->controller . DS . 'footer.php')) {
             include (ROOT  . 'app' . DS . 'todo' . DS . 'view' . DS . $this->controller . DS . 'footer.php');
         } else {
--- lib/core/model/Base.php
+++ lib/core/model/Base.php
@@ -1,14 +1,14 @@
 <?php
 
-class Core_Model_Base extends Core_Model_SQLQuery {
+abstract class Core_Model_Base extends Core_Model_SQLQuery {
 
     protected $model;
 
     public function __construct() {
         $this->connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
-        $temp = get_class($this);
-        $temp2 = explode('_',$temp);
-        $this->model = end($temp2);
+        $className = get_class($this);
+        $temp = explode('_',$className);
+        $this->model = end($temp);
         $this->table = strtolower($this->model) . 's';
     }
 }
