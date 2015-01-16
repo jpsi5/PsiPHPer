f97f9ade82905c2a31d00b4e7edcab6eb4f1d341
jpsi5 <jsimon@clemson.edu>
2015-01-16 14:12:56 -0500

    Added controllers Connect & Web Added user information storing functionality during registration

---
+++ app/dark/controller/Web.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jsimon
+ * Date: 1/16/15
+ * Time: 11:47 AM
+ */ 
---
+++ app/dark/view/block/Page.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jsimon
+ * Date: 1/16/15
+ * Time: 9:11 AM
+ */ 
